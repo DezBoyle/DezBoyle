c2e5a55aed3c4c122a128949526035a19ee5d4a9
Dez Boyle <41555304+DezBoyle@users.noreply.github.com>
2023-07-05 14:00:55 -0500

    Update AudioPlayer.cs

--- Code/Godot/AudioPlayer.cs
+++ Code/Godot/AudioPlayer.cs
@@ -27,7 +27,7 @@ public partial class AudioPlayer : Node3D
 	{
 		currentAudio.Stream = stream;
 		currentAudio.Play();
-		currentAudio.VolumeDb = volumeDb - 100; //bug in godot.  100 = 0Db apparently
+		currentAudio.VolumeDb = volumeDb;
 		currentAudio.PitchScale = pitch;
 		currentAudio.Finished += currentAudio.QueueFree;
 		NewAudio();
