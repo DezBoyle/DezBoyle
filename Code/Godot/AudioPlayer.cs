19db498aae783a338d1e4afa68ef36b5a2dae9d3
Dez Boyle <41555304+DezBoyle@users.noreply.github.com>
2026-09-12 20:24:20 -0500

    Update AudioPlayer.cs

--- Code/Godot/AudioPlayer.cs
+++ Code/Godot/AudioPlayer.cs
@@ -1,5 +1,6 @@
 using Godot;
 using System;
+using System.Collections.Generic;
 
 /// <summary>
 /// An Audio Player script that adds more functionality onto Godot's AudioStreamPlayer3D
@@ -7,35 +8,127 @@ using System;
 /// 		   A "PlayOneShot" method which is similar to Unity
 /// 		   Random Sounds
 /// </summary>
+[GlobalClass]
 public partial class AudioPlayer : Node3D
 {
-	private AudioStreamPlayer3D currentAudio;
+	[Export] public AudioStream ClipToPlayOnAwake = null;
+	[Export] private float clipToPlayOnAwakeVolume = 0f;
+	[Export] private float clipToPlayOnAwakePitchMin = 1f;
+	[Export] private float clipToPlayOnAwakePitchMax = 1f;
+	[Export] private bool randomStartPos = false;
 
-	public override void _EnterTree()
+	[Export] public float MaxDistance = 30f;
+	[Export] public bool Is3D { get; set; } = true;
+    public AudioStreamPlayer3D PlayOnAwakeAudioSource { get; private set; }
+
+	private AudioStreamPlayer3D currentAudio3D; //sadly, AudioStreamPlayer3D doesnt inherit from AudioStreamPlayer :(
+	private AudioStreamPlayer currentAudio;
+
+	public AudioPlayer() {} //Godot requires an empty constructor
+	public AudioPlayer(bool is3D)
+	{
+		Is3D = is3D;
+	}
+
+	public const float DefaultMaxDistance = 30;
+
+	public override void _Ready()
 	{
 		NewAudio();
+
+		if (ClipToPlayOnAwake != null)
+		{
+			PlayOneShot(ClipToPlayOnAwake, clipToPlayOnAwakeVolume, Utility.RandomRange(clipToPlayOnAwakePitchMin, clipToPlayOnAwakePitchMax),
+						randomStartPos ? Utility.RandomRange(0f, (float)ClipToPlayOnAwake.GetLength()) : 0f);
+			PlayOnAwakeAudioSource = currentAudio3D; //assuming its 3D because you cant change Is3D in the inspector
+		}
 	}
 
-	public void PlayRandom(Godot.Collections.Array<AudioStream> streams, float volumeDb)
+
+	private int lastRandomIndex = -1;
+	public void PlayOneShotRandom(Godot.Collections.Array<AudioStream> streams, float volumeDb, float pitch = 1f)
 	{
-		PlayOneShot(streams[Utility.RandomRange(0, streams.Count)], volumeDb);
+		int random = Utility.RandomRange(0, streams.Count);
+		if(random == lastRandomIndex)
+		{
+			random = (random + 1) % streams.Count; //add 1 or wrap around to 0
+		}
+		lastRandomIndex = random;
+		PlayOneShot(streams[random], volumeDb, pitch);
 	}
-
-  	public void PlayOneShot(AudioStream stream, float volumeDb) { PlayOneShot(stream, volumeDb, 1f); }
-	public void PlayOneShot(AudioStream stream, float volumeDb, float pitch)
+	public void PlayOneShotRandom(AudioStream[] streams, float volumeDb, float pitch = 1f)
 	{
-		currentAudio.Stream = stream;
-		currentAudio.Play();
-  		currentAudio3D.VolumeDb = volumeDb;
-		currentAudio3D.MaxDb = volumeDb;
-		currentAudio.PitchScale = pitch;
-		currentAudio.Finished += currentAudio.QueueFree;
-		NewAudio();
+		int random = Utility.RandomRange(0, streams.Length);
+		if(random == lastRandomIndex)
+		{
+			random = (random + 1) % streams.Length; //add 1 or wrap around to 0
+		}
+		lastRandomIndex = random;
+		PlayOneShot(streams[random], volumeDb, pitch);
 	}
 
+    public void PlayOneShot(AudioStream stream, float volumeDb) { PlayOneShot(stream, volumeDb, 1f); }
+	public void PlayOneShot(AudioStream stream, float volumeDb, float pitch, float startPos = 0f)
+	{
+		if(Is3D)
+		{
+            currentAudio3D.Stream = stream;
+            currentAudio3D.Seek(startPos);
+            currentAudio3D.Play();
+            currentAudio3D.VolumeDb = volumeDb;
+			currentAudio3D.MaxDb = volumeDb;
+            currentAudio3D.PitchScale = pitch;
+            currentAudio3D.Finished += currentAudio3D.QueueFree;
+            NewAudio();
+		}
+		else
+		{
+			currentAudio.Stream = stream;
+            currentAudio.Seek(startPos);
+            currentAudio.Play();
+            currentAudio.VolumeDb = volumeDb;
+            currentAudio.PitchScale = pitch;
+            currentAudio.Finished += currentAudio.QueueFree;
+            NewAudio();
+		}
+		
+	}
+
+	public void StopPlaying()
+	{
+		foreach (Node child in GetChildren())
+		{
+			if(child == currentAudio || child == currentAudio3D)
+			{ return; }
+			
+			AudioStreamPlayer audio = child as AudioStreamPlayer;
+			AudioStreamPlayer3D audio3D = child as AudioStreamPlayer3D;
+			audio?.QueueFree();
+			audio3D?.QueueFree();
+		}
+	}
+
+	private int id = 0;
 	private void NewAudio()
 	{
-		currentAudio = new AudioStreamPlayer3D();
-		AddChild(currentAudio);
+		if (Is3D)
+		{
+			currentAudio3D = new AudioStreamPlayer3D();
+			currentAudio3D.AttenuationFilterCutoffHz = 20500;
+			currentAudio3D.MaxDistance = MaxDistance;
+			AddChild(currentAudio3D);
+			currentAudio3D.Position = Vector3.Zero;
+			currentAudio3D.Name = $"AudioStreamPlayer3D_{id}";
+		}
+		else
+		{
+			currentAudio = new AudioStreamPlayer();
+			AddChild(currentAudio);
+			currentAudio.Name = $"AudioStreamPlayer_{id}";
+		}
+
+		id++;
+		if(id >= int.MaxValue)
+		{ id = 0;}
 	}
 }
